cba356fbe6dab53b284d3a570398d683f56d22d4
Tan I-hur <ihur.tan@gmail.com>
2026-05-08 16:55:50 +0800

    Implement item squash and stretch

--- scripts/Stubby/stubby.gd
+++ scripts/Stubby/stubby.gd
@@ -1,12 +1,16 @@
 extends RigidBody2D
 class_name Stubby
 
-var dragging := false
-var hovered := false
+@onready var sprite := get_node("Sprite2D")
 
+@export var squash_strength := 1.2
 @export var follow_speed := 35.0
 @export var release_velocity_multiplier := 1.0
 
+var dragging := false
+var hovered := false
+
+var target_scale := Vector2.ONE
 var tracked_velocity := Vector2.ZERO
 var last_position := Vector2.ZERO
 
@@ -29,6 +33,7 @@ func _input(event):
 				linear_velocity = tracked_velocity * release_velocity_multiplier
 
 func _physics_process(delta):
+	
 	if dragging:
 		var mouse_pos = get_global_mouse_position()
 		# Smooth direct movement
@@ -39,8 +44,46 @@ func _physics_process(delta):
 
 		# Track velocity for throwing
 		tracked_velocity = (global_position - last_position) / delta
+
+	# Squash/Stretch code
+	sprite.scale = scale.lerp(target_scale, delta * 15.0)
+	# Slowly return to normal size
+	target_scale = target_scale.lerp(Vector2.ONE, delta * 10.0)
+	
 	last_position = global_position
 
+func _integrate_forces(state):
+	for i in state.get_contact_count():
+		var normal = state.get_contact_local_normal(i)
+
+		# Normalize collision force
+		var x_force = clamp(abs(linear_velocity.x) / 600.0, 0.0, 1.0)
+		var y_force = clamp(abs(linear_velocity.y) / 600.0, 0.0, 1.0)
+		# Floor / ceiling hit
+		if abs(normal.y) > 0.7:
+			squash(y_force, false)
+
+		# Wall hit
+		elif abs(normal.x) > 0.7:
+			squash(x_force, true)
+
+		# Only trigger once per frame
+		break
+
+func squash(force : float, vertical : bool) -> void:
+
+	var squash_amount = force * squash_strength
+
+	if !vertical:
+		target_scale = Vector2(
+			1.0 + squash_amount,
+			1.0 - squash_amount
+		)
+	else:
+		target_scale = Vector2(
+			1.0 - squash_amount,
+			1.0 + squash_amount
+		)
 
 func _on_area_mouse_entered() -> void:
 	hovered = true
--- scripts/main.gd
+++ scripts/main.gd
@@ -4,11 +4,6 @@ class_name Main
 func _ready():
 	update_window_size()
 
-func _notification(what):
-	if what == NOTIFICATION_WM_SIZE_CHANGED:
-		# Optional: if you want to react to manual resizing too
-		update_window_size()
-
 func update_window_size():
 	var screen_size = DisplayServer.screen_get_usable_rect().size
 	DisplayServer.window_set_size(screen_size)
