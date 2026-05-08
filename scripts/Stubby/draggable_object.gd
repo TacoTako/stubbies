ff814b4fcdb25bfa8b586455f9a0f000bac66619
Tan I-hur <ihur.tan@gmail.com>
2026-05-08 17:00:06 +0800

    Abstract out draggable object class

---
+++ scripts/Stubby/draggable_object.gd
@@ -0,0 +1,50 @@
+extends RigidBody2D
+class_name DraggableObject
+
+@export var follow_speed := 35.0
+@export var release_velocity_multiplier := 1.0
+
+var dragging := false
+var hovered := false
+
+var tracked_velocity := Vector2.ZERO
+var last_position := Vector2.ZERO
+
+func _ready() -> void:
+	last_position = global_position
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+
+			# START DRAG
+			if event.pressed and hovered:
+				dragging = true
+				freeze = true
+
+			# RELEASE DRAG
+			elif !event.pressed and dragging:
+				dragging = false
+				freeze = false
+				linear_velocity = tracked_velocity * release_velocity_multiplier
+
+func _physics_process(delta):
+	if dragging:
+		var mouse_pos = get_global_mouse_position()
+		# Smooth direct movement
+		global_position = global_position.lerp(
+			mouse_pos,
+			1.0 - exp(-follow_speed * delta)
+		)
+
+		# Track velocity for throwing
+		tracked_velocity = (global_position - last_position) / delta
+	
+	last_position = global_position
+
+func _on_area_mouse_entered() -> void:
+	hovered = true
+
+
+func _on_area_mouse_exited() -> void:
+	hovered = false
--- scripts/Stubby/stubby.gd
+++ scripts/Stubby/stubby.gd
@@ -1,56 +1,20 @@
-extends RigidBody2D
+extends DraggableObject
 class_name Stubby
 
 @onready var sprite := get_node("Sprite2D")
 
 @export var squash_strength := 1.2
-@export var follow_speed := 35.0
-@export var release_velocity_multiplier := 1.0
-
-var dragging := false
-var hovered := false
 
 var target_scale := Vector2.ONE
-var tracked_velocity := Vector2.ZERO
-var last_position := Vector2.ZERO
 
 func _ready():
-	last_position = global_position
+	pass
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-
-			# START DRAG
-			if event.pressed and hovered:
-				dragging = true
-				freeze = true
-
-			# RELEASE DRAG
-			elif !event.pressed and dragging:
-				dragging = false
-				freeze = false
-				linear_velocity = tracked_velocity * release_velocity_multiplier
-
-func _physics_process(delta):
-	
-	if dragging:
-		var mouse_pos = get_global_mouse_position()
-		# Smooth direct movement
-		global_position = global_position.lerp(
-			mouse_pos,
-			1.0 - exp(-follow_speed * delta)
-		)
-
-		# Track velocity for throwing
-		tracked_velocity = (global_position - last_position) / delta
-
+func _process(delta):
 	# Squash/Stretch code
 	sprite.scale = scale.lerp(target_scale, delta * 15.0)
 	# Slowly return to normal size
 	target_scale = target_scale.lerp(Vector2.ONE, delta * 10.0)
-	
-	last_position = global_position
 
 func _integrate_forces(state):
 	for i in state.get_contact_count():
@@ -71,7 +35,6 @@ func _integrate_forces(state):
 		break
 
 func squash(force : float, vertical : bool) -> void:
-
 	var squash_amount = force * squash_strength
 
 	if !vertical:
@@ -84,10 +47,3 @@ func squash(force : float, vertical : bool) -> void:
 			1.0 - squash_amount,
 			1.0 + squash_amount
 		)
-
-func _on_area_mouse_entered() -> void:
-	hovered = true
-
-
-func _on_area_mouse_exited() -> void:
-	hovered = false
