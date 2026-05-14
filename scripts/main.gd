0998e0c016b28ae807492dbb9f6bde16edf7f9e9
Tan I-hur <ihur.tan@gmail.com>
2026-05-15 01:53:05 +0800

    Fix window cut off bug
    When entering fullscreen for other applications, the window would cut off at the top.
    
    Expanded window height and shifted it downwards to expand window slightly higher to compensate

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,7 +8,8 @@ func _ready():
 	update_stage_size()
 
 func update_stage_size():
-	var size := ScreenSize.screen_size
+	var size := ScreenSize.screen_size + Vector2i(0, 10)
 	DisplayServer.window_set_size(size)
+	DisplayServer.window_set_position(Vector2i(0, -10))
 	self.floor.position.y = size.y
 	self.right_wall.position.x = size.x
